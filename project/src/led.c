99b58d8256d7682b87151f437dc7cb2c1eda45bf
Eder Rodriguez <ederrod21@gmail.com>
2018-10-25 15:42:13 -0600

    Fix up

--- project/src/led.c
+++ project/src/led.c
@@ -1,35 +1,24 @@
 #include <msp430.h>
 #include "led.h"
-
-unsigned char led_state = 0;
-unsigned char count = 0;
-unsigned char led_changed = 0;
+#include "switches.h"
 
 void led_init()
 {
-  P1DIR |= LEDS;
-  led_changed = 1;
-  led_update(); 
+  P1DIR |= LEDS;		// bits attached to leds are output
+  switch_state_changed = 1;
+  led_update();
 }
 
-void led_update()
-{
-  if (led_changed)
-  {
-    char led_flags = 0;
-    if (led_state)
-    {
-      led_flags != LED_GREEN; 
-    }
+void led_update(){
+  if (switch_state_changed) {
+    char ledFlags = 0; /* by default, no LEDs on */
 
-    P1OUT &= (0xff^LEDS) | led_flags;
-    P1OUT |= led_flags;
-    led_changed = 0; 
+    ledFlags |= switch_state_down ? LED_GREEN : 0;
+    ledFlags |= switch_state_down ? 0 : LED_RED;
+
+    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
+    P1OUT |= ledFlags;         // set bits for on leds
   }
+  switch_state_changed = 0;
 }
 
-void led_toggle()
-{
-  led_state = 1 - led_state;
-  led_changed = 1; 
-}
