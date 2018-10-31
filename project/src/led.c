cf926e5bfaafab6d0c0b7746ca103edee3c2d574
Eder Rodriguez <ederrod21@gmail.com>
2018-10-30 21:19:20 -0600

    switches interrupt change

--- project/src/led.c
+++ project/src/led.c
@@ -1,30 +1,24 @@
 #include <msp430.h>
 #include "led.h"
-
-unsigned char led_state = 0;
-unsigned char count = 0;
-unsigned char led_changed = 0;
+#include "switches.h"
 
 void led_init()
 {
-  P2DIR |= LEDS;		// bits attached to leds are output
-  led_changed = 1;
+  P1DIR |= LEDS;		// bits attached to leds are output
+  switch_state_changed = 1;
   led_update();
 }
 
-void led_update()
-{
-  if (led_changed) {
-    char ledFlags = 0;
-    if(led_state)
-      ledFlags |= LED_GREEN;
-    P2OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
-    P2OUT |= ledFlags;		     // set bit for on leds
-    led_changed = 0;
+void led_update(){
+  if (switch_state_changed) {
+    char ledFlags = 0; /* by default, no LEDs on */
+
+    ledFlags |= switch_state_down ? LED_GREEN : 0;
+    ledFlags |= switch_state_down ? 0 : LED_RED;
+
+    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
+    P1OUT |= ledFlags;         // set bits for on leds
   }
+  switch_state_changed = 0;
 }
 
-void led_toggle() {
-  led_state = 1 - led_state;
-  led_changed = 1;
-}
