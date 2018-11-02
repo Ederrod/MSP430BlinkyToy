bd1b5e689e23d1fc78b502315e542b5e63a000ca
Eder Rodriguez <ederrod21@gmail.com>
2018-11-01 22:01:35 -0600

    fixing bug

--- project/src/statemachine.c
+++ project/src/statemachine.c
@@ -24,13 +24,11 @@ led_state_update()
     case SW2: /*DIM_RED*/
         // turn red on
         red_on = 1; 
-        green_on = 0; 
-        // if (green_on)
-        // {
-        //     led_changed = 1;
-        //     green_on = 0; 
-        //     red_on = 1; 
-        // }
+        if (green_on)
+        {
+            led_changed = 1;
+            green_on = 0; 
+        }
         led_dim_state_update();
         //buzzer_advance_frequency(); 
         break; 
@@ -48,14 +46,11 @@ led_state_update()
     case SW4: /*DIM_GREEN*/
         // turn green on 
         green_on = 1; 
-        red_on = 0; 
-        led_dim_state_update();
-        // if (red_on)
-        // {
-        //     led_changed = 1;
-        //     green_on = 1; 
-        //     red_on = 0; 
-        // }
+        if (red_on)
+        {
+            led_changed = 1;
+            red_on = 0; 
+        }
         //buzzer_advance_frequency();
         break; 
     }
