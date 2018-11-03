8e38a2b2d264c635f92299cf24c093fdf0770911
Eder Rodriguez <ederrod21@gmail.com>
2018-11-02 20:25:42 -0600

    found a bug, bug fix

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -1,6 +1,7 @@
 #include <msp430.h>
 #include "led.h"
 #include "switches.h"
+#include "led_dim_statemachine.h"
 
 
 /**
@@ -10,8 +11,8 @@
  */
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static char blink_count = 0;
   if(switch_btn == SW2 || switch_btn == SW4){
+    static char blink_count = 0;
     if (++blink_count == 125) {
       led_dim_slow_clock(); 
       blink_count = 0;
