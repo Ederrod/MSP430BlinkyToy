32c76855f6a0ea332aef653ca1afb7b754c1a215
Eder Rodriguez <ederrod21@gmail.com>
2018-11-04 15:14:41 -0700

    adding fixes

--- project/src/statemachine.s
+++ project/src/statemachine.s
@@ -1,52 +1,49 @@
 .text
-
-jt: .word case1  ; jt[1] 
-    .word case2  ; jt[2]
-    .word case3  ; jt[3]
-    .word case4  ; jt[4]
-
+    .balign 2
     .global led_state_update
     .type led_state_update, @function
 
 led_state_update: 
-    cmp.b #4, &switch_btn
-    jnc   end
-    
     mov.b &switch_btn, r12
-    add   r12, r12
-    mov   jt(r12), r0
-
-case1: 
+    cmp.w #2,  r12 { jeq .L4
+    mov.b #2,  r13
+    cmp.w r12, r13 { jl  .L2
+    cmp.w #1,  r12 { jeq .L3
+.L2: 
+    cmp.w #4, r12 { jeq .L5
+    cmp.w #8, r12 { jeq .L6
+    br    #.L7
+.L3:;SW1
     mov.b #1, &led_changed
     mov.b #0, &green_on
     mov.b #1, &red_on
     call  #buzzer_advance_frequency
-    jmp   end
-case2: 
+    br    #.L7
+.L4:;SW2
     mov.b #1, &led_changed
     mov.b #1, &red_on
     mov.b #0, &green_on
     call  #led_dim_state_update
     call  #buzzer_advance_frequency
-    jmp   end
-case3: 
+    br    #.L7
+.L5:;SW3
     mov.b #1, &led_changed
     mov.b #1, &green_on
     mov.b #0, &red_on
     call  #buzzer_advance_frequency
-    jmp   end
-case4: 
+    br    #.L7
+.L6: ;SW4 
     mov.b #1, &led_changed
     mov.b #0, &red_on
     mov.b #1, &green_on
     call  #led_dim_state_update
     call  #buzzer_advance_frequency
-    jmp   end
+    br    #.L7
 
-end: 
+.L7: 
     mov.b #0, &switch_state_down
     mov.b #0, switch_state_up
-    call #led_state_update
+    call #led_update
     nop
     ret
     .size led_state_update, .-led_state_update
