333053cf06eae23aec524731ed33fb0817468052
Eder Rodriguez <ederrod21@gmail.com>
2018-11-04 15:40:43 -0700

    milestone

--- project/src/statemachine.s
+++ project/src/statemachine.s
@@ -6,34 +6,34 @@
     .global led_state_update
 led_state_update: 
     mov.b &switch_btn, r12
-    cmp.w #2,  r12 { jeq .L4
+    cmp.w #2,  r12 { jeq .STATE2 ; Jump to state 2
     mov.b #2,  r13
-    cmp.w r12, r13 { jl  .L2
-    cmp.w #1,  r12 { jeq .L3
+    cmp.w r12, r13 { jl  .L2 ; Jump to check for state 3 and 4
+    cmp.w #1,  r12 { jeq .STATE1 ; Jump to state 1
 .L2: 
-    cmp.w #4, r12 { jeq .L5
-    cmp.w #8, r12 { jeq .L6
+    cmp.w #4, r12 { jeq .STATE3  ; Jump to state 3
+    cmp.w #8, r12 { jeq .STATE4  ; Jump to state 4
     br    #.L7
-.L3:;SW1
+.STATE1:;SW1
     mov.b #1, &led_changed
     mov.b #0, &green_on
     mov.b #1, &red_on
     call  #buzzer_advance_frequency
     br    #.L7
-.L4:;SW2
+.STATE2:;SW2
     mov.b #1, &led_changed
     mov.b #1, &red_on
     mov.b #0, &green_on
     call  #led_dim_state_update
     call  #buzzer_advance_frequency
     br    #.L7
-.L5:;SW3
+.STATE3:;SW3
     mov.b #1, &led_changed
     mov.b #1, &green_on
     mov.b #0, &red_on
     call  #buzzer_advance_frequency
     br    #.L7
-.L6: ;SW4 
+.STATE4: ;SW4 
     mov.b #1, &led_changed
     mov.b #0, &red_on
     mov.b #1, &green_on
